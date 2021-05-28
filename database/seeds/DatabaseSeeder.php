f029e49f8903898700d526381bf76064e6bbc087
Matteo <grecomatteo86@gmail.com>
2021-05-28 17:07:30 +0200

    Tag Seeder Creation ;)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PostsTableSeeder::class, CommentsTableSeeder::class]);
+        $this->call([PostsTableSeeder::class, CommentsTableSeeder::class, TagsTableSeeder::class]);
     }
 }
